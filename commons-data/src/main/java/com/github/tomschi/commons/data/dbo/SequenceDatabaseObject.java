4b7ae6c7097efaaaac392eaf693550deb9a9b785
Tomschi <tomschi@users.noreply.github.com>
2017-12-05 07:29:20 +0100

    Add new interface SequenceDatabaseObject

---
+++ commons-data/src/main/java/com/github/tomschi/commons/data/dbo/SequenceDatabaseObject.java
@@ -0,0 +1,11 @@
+package com.github.tomschi.commons.data.dbo;
+
+/**
+ * The interface {@link SequenceDatabaseObject} is the base interface
+ * for all database objects with a {@link Long} as id.
+ *
+ * @author Tomschi
+ * @since 0.1.1
+ */
+public interface SequenceDatabaseObject extends DatabaseObject<Long> {
+}
