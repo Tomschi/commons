6abf5e020b70eb1ff3271ecb4cbc0215ff3b3507
Tomschi <tomschi@users.noreply.github.com>
2018-01-26 08:09:22 +0100

    Create interface ModelObject.

---
+++ commons-data/src/main/java/com/github/tomschi/commons/data/model/ModelObject.java
@@ -0,0 +1,13 @@
+package com.github.tomschi.commons.data.model;
+
+import java.io.Serializable;
+
+/**
+ * The interface {@link ModelObject} is the base interface for model
+ * objects.
+ *
+ * @author Tomschi
+ * @since 0.1.2
+ */
+public interface ModelObject extends Serializable {
+}
