4545e9f960907e9e48f7440201d85379eeea176d
Tomschi <codingtomschi@gmail.com>
2019-10-07 07:36:34 +0200

    Remove extends Serializable from RESTDataTransferObject.

--- commons-data/src/main/java/com/github/tomschi/commons/data/dto/rest/RESTDataTransferObject.java
+++ commons-data/src/main/java/com/github/tomschi/commons/data/dto/rest/RESTDataTransferObject.java
@@ -21,8 +21,6 @@ package com.github.tomschi.commons.data.dto.rest;
 
 import com.github.tomschi.commons.data.dto.DataTransferObject;
 
-import java.io.Serializable;
-
 /**
  * The {@link RESTDataTransferObject} interface is the base interface
  * for all rest data transfer objects.
@@ -30,5 +28,5 @@ import java.io.Serializable;
  * @since 0.1.0
  * @author Tomschi
  */
-public interface RESTDataTransferObject extends DataTransferObject, Serializable {
+public interface RESTDataTransferObject extends DataTransferObject {
 }
